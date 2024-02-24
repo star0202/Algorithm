53e1352348717f7abe22601614e041099cafac62
Starcea <stardev.uwu@gmail.com>
2024-02-25 01:24:51 +0900

    feat: update python template

--- templates/ps.py
+++ templates/ps.py
@@ -1,9 +1,14 @@
-stdin = open(0)
+from sys import stdin, stdout
+
 input = lambda: stdin.readline().rstrip()
+_out: list[str] = []
+print = lambda *v, sep=" ", end="\n": _out.append(sep.join(map(str, v)) + end)
 
 
 def main() -> None:
     # code here
+
+    stdout.write("".join(_out))
 
 
 if __name__ == "__main__":
