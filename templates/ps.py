9dcb10be4b380177f2c53a9c48a330dc4f6facee
Starcea <stardev.uwu@gmail.com>
2024-02-25 19:31:02 +0900

    feat: update python template

--- templates/ps.py
+++ templates/ps.py
@@ -8,8 +8,8 @@ print = lambda *v, sep=" ", end="\n": _out.append(sep.join(map(str, v)) + end)
 def main() -> None:
     # code here
 
-    stdout.write("".join(_out))
-
 
 if __name__ == "__main__":
     main()
+
+    stdout.write("".join(_out))
