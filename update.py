d87bdf5815db7b40de1922a14b1829008ce4ed98
Starcea <stardev.uwu@gmail.com>
2024-03-22 21:31:35 +0900

    feat(snippets): code hash

--- update.py
+++ update.py
@@ -1,6 +1,7 @@
-from json import dumps
+from json import dumps, loads
 from os import listdir, path
 from typing import TypedDict
+from hashlib import sha256 as _sha256
 
 DIR = "templates"
 COMMENT_MAPPING = {
@@ -18,6 +19,11 @@ class SnippetSchema(TypedDict):
     scope: str
     prefix: str
     body: list[str]
+    hash: str
+
+
+def sha256(data: str) -> str:
+    return _sha256(data.encode()).hexdigest()
 
 
 def comment(ext: str) -> str:
@@ -37,6 +43,10 @@ def language(ext: str) -> str:
 def main():
     json: dict[str, SnippetSchema] = {}
 
+    if path.exists(SNIPPET_FILE):
+        with open(SNIPPET_FILE, "r") as f:
+            json = loads(f.read())
+
     for file in listdir(DIR):
         full_path = path.join(DIR, file)
 
@@ -48,11 +58,18 @@ def main():
 
         with open(full_path, "r") as f:
             code = f.read()
+        
+        sha = sha256(code)
+
+        if json.get(f"{name}-{ext}", {}).get("hash") == sha:
+            print(f"Skipped {full_path}")
+            continue
 
         json[f"{name}-{ext}"] = {
             "scope": language(ext),
             "prefix": name,
             "body": code.replace(f"{comment(ext)} code here", "$0").splitlines() + [""],
+            "hash": sha,
         }
 
         print(f"Updated {full_path}")
